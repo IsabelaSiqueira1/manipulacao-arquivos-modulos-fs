1f5e1e206069996be8e6881aa76859e5abe7ac8f
Isabela Siqueira <80498863+IsabelaSiqueira1@users.noreply.github.com>
2024-06-14 21:57:39 -0300

    manipulação de arquivos com modulo "fs"

---
+++ read-file.js
@@ -0,0 +1,28 @@
+const fs = require('node:fs')
+
+const filename = "arquivo.csv"
+const exists = fs.existsSync(filename)
+
+if (exists) {
+  fs.readFile(filename, "utf-8", (error, data) => {
+    if (error) {
+      console.log("Erro ao ler o arquivo: ", error.message)
+      return
+    }
+  
+    const entries = data.split(",")
+    console.log(entries)
+    entries.forEach((entry) => console.log(entry))
+  })
+} else {
+  console.log("O arquivo não existe!")
+}
+
+// readFile de forma síncrona
+
+// try {
+//   const data = fs.readFileSync("./arquivo.txt", "utf-8")
+//   console.log(data)
+// } catch (error) {
+//   console.log("Erro ao ler o arquivo: ", error.message)
+// }
